1f9e5f60799d979d225b93d7c578c29574821d31
Freelancer-Md <freelancingdotmax@gmail.com>
2025-07-30 00:12:56 +0530

    adjusted /

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://sales-dashboard-frontend.netlify.app/'],
+  origin: ['http://localhost:5173', 'https://sales-dashboard-frontend.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
